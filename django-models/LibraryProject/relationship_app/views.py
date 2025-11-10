0cffc924d6c58406dc0b86fbea12b14dd0274ad0
Omondi Danish <omondidanish77@gmail.com>
2025-11-10 16:49:21 +0300

    reshufling views.py with relevant information

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -8,11 +8,11 @@ from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 def list_books(request):
     books = Book.objects.all()
-    return render(request, "list_books.html", {"books": books})
+    return render(request, "relationship_app/list_books.html", {"books": books})
 
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = "library_detail.html"
+    template_name = "relationship_app/library_detail.html"
     context_object_name = "library"
     
 # Function-based view for login
